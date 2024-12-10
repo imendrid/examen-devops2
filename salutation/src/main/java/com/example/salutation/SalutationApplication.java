008b49bd637a2a0f9cb40e68dcdc876e64bef15e
imendrid <deridiimen@gmail.com>
2024-12-10 10:21:42 +0100

    Initial commit for examen-devops project

---
+++ salutation/src/main/java/com/example/salutation/SalutationApplication.java
@@ -0,0 +1,13 @@
+package com.example.salutation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalutationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalutationApplication.class, args);
+	}
+
+}
